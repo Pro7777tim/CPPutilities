e9dcdf2b3720ac3a85b2b40085e094a31d8c965f
Pro7777tim <khtim1226@gmail.com>
2025-10-08 20:46:57 +0300

    Bug fixes

--- source/scr/py/timerWindow.py
+++ source/scr/py/timerWindow.py
@@ -1,2 +1,23 @@
-from tkinter.messagebox import*
-showwarning('WARNING', 'Timer end!');
+import os
+import tkinter as tk
+from tkinter import messagebox
+
+def gui_available():
+    # Перевіряємо базову змінну оточення
+    if "DISPLAY" in os.environ or "WAYLAND_DISPLAY" in os.environ:
+        return True
+    return False
+
+def show_warning_if_gui():
+    if gui_available():
+        try:
+            root = tk.Tk()
+            root.withdraw()  # сховати головне вікно
+            messagebox.showwarning("WARNING", "Timer end!")
+            root.destroy()
+        except Exception as e:
+            pass
+
+# Виклик:
+show_warning_if_gui()
+
